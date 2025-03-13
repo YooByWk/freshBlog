bff1a14806e7ce5b123a5a3ce98b7ade8ef4c895
YooByWk <tls19190@naver.com>
2025-03-14 05:08:42 +0900

    fix: GET 요청에 대한 로그 출력 형식 개선

--- first-fresh-blog/routes/_middleware.ts
+++ first-fresh-blog/routes/_middleware.ts
@@ -3,7 +3,6 @@ import { FreshContext } from "$fresh/server.ts";
 export async function handler(req: Request, ctx: FreshContext) {
   const url = new URL(req.url);
   const path = url.pathname;
-
   // 정적 파일 확장자 목록
   const staticExtensions = [
     ".js",
@@ -16,9 +15,15 @@ export async function handler(req: Request, ctx: FreshContext) {
   ];
 
   // 정적 파일이 아니면 로그 출력
-  if (!staticExtensions.some((ext) => path.endsWith(ext))) {
-    console.log(`🖥️  ${req.headers.get("x-real-ip")} ::  🔗 - ${req.url}`);
+  if (
+    req.method === "GET" && !staticExtensions.some((ext) => path.endsWith(ext))
+  ) {
+    console.log(
+      `  🖥️  %c${req.headers.get("x-real-ip")} %c::  🔗  %c- ${req.url}`,
+      "color:red",
+      "color:blue",
+      "color:blue; font-weight:bold",
+    );
   }
-
   return await ctx.next();
 }
