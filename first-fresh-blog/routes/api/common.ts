9c0ad2259a0f15e3f4a1e486af9712cf02a1ebaf
유병욱 <tls19190@naver.com>
2025-05-15 03:53:45 +0900

    실험: 배포 주소 체크

--- first-fresh-blog/routes/api/common.ts
+++ first-fresh-blog/routes/api/common.ts
@@ -3,7 +3,8 @@ import { createFetch } from "./image.ts";
 interface P {
   encoded: string;
 }
-const URL = Deno.env.get("URL") || 'http://localhost:3000/api/';
+// const URL = Deno.env.get("URL") || 'http://localhost:3000/api/';
+const URL = 'http://localhost:3000/api/';
 const api = createFetch(URL);
 console.log(URL);
 
